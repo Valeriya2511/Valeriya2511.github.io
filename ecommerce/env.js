336d33195e85e06bd72083df8494cc97ddab6962
AlexDrags <47152229+AlexDrags@users.noreply.github.com>
2023-08-21 14:44:48 +0300

    Add files via upload

---
+++ ecommerce/env.js
@@ -0,0 +1,5 @@
+window.env = {
+  "AUTORIZATION": "Basic RUpIYUNleVVxTjYwUHBhQzBNUEM4MExzOkJuVkkxdDlwWHc2SG9PQ1ozN0EtWWptaU5ZbVlwa2dH",
+  "AUTH_URL": "https://auth.australia-southeast1.gcp.commercetools.com",
+  "SCOPES": "manage_project:ecommerce-application_1"
+};
