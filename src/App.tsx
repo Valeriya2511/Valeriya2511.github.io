53812f5cb4c22b4d889135016020009bb1c092b7
error404-2 <kapwas@mail.ru>
2023-08-21 22:07:22 +0300

    feat: add setIsAuth in useContext, add 404 Page, fix links, add routes for 404

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
 import { Navigation } from './components/navigation/Navigation';
 import { getToken } from './ecommerceAPI/getToken';
 import { RouterApp } from './components/router/RouterApp';
+import { AuthContext } from './context/authContext/AuthContext';
+import { useState } from 'react';
 
 getToken();
 function App() {
+  const [isAuth, setIsAuth] = useState(false);
   return (
-    <>
+    <AuthContext.Provider
+      value={{
+        isAuth,
+        setIsAuth,
+      }}
+    >
       <Navigation />
       <RouterApp />
-    </>
+    </AuthContext.Provider>
   );
 }
 
