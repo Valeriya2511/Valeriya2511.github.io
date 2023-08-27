75b651f4e480ce5f16abf82c95f2a22647c03dc6
error404-2 <kapwas@mail.ru>
2023-08-27 22:41:10 +0300

    feat: get and save product list 50sku

--- src/ecommerceAPI/GetProductData.ts
+++ src/ecommerceAPI/GetProductData.ts
@@ -1,6 +1,6 @@
 export const getProductData = async () => {
   console.log('гет дата продукт');
-  const url: string = 'https://api.australia-southeast1.gcp.commercetools.com/ecommerce-application_1/products/';
+  const url: string = 'https://api.australia-southeast1.gcp.commercetools.com/ecommerce-application_1/categories/';
 
   const data = await fetch(`${url}`, {
     method: 'GET',
@@ -8,9 +8,11 @@ export const getProductData = async () => {
       Authorization: 'Bearer ' + 'wXJKzLGjU8lE3-MzIJlHPeiTbsxBe28M',
       'Content-Type': 'application/x-www-form-urlencoded',
     },
-
+  
   }).then(res => res.json());
-
-  console.log('авэйт дата продукт', data.results[1].masterData.current.name.en);
+console.log('дата', data);
+  console.log('авэйт дата продукт', data.results);
+// const {masterVariant} = data.results[1].masterData.current
+//   console.log('masterVariant', masterVariant.attributes);
   return await data;
 };
