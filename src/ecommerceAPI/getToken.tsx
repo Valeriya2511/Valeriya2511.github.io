2287dcbfe0e07ee7bfd8cbbdcab52df13042979b
Valeriya2511 <finsternis.ru@mail.ru>
2023-08-29 20:53:04 +0600

    feat: fixed before upload

--- src/ecommerceAPI/getToken.tsx
+++ src/ecommerceAPI/getToken.tsx
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-import env from 'react-dotenv';
-import axios from 'axios';
-
-
-export const getToken = async () => {
-  const data = await fetch(`${env.AUTH_URL}/oauth/token?grant_type=client_credentials&scope=${env.SCOPES}`, {
-    method: 'POST',
-    headers: {
-      Authorization: `${env.AUTORIZATION}`,
-=======
 export const getToken = async () => {
   const codeStr = window.btoa('EJHaCeyUqN60PpaC0MPC80Ls:BnVI1t9pXw6HoOCZ37A-YjmiNYmYpkgG');
   const data = await fetch(
@@ -19,7 +8,6 @@ export const getToken = async () => {
         Authorization: 'Basic ' + codeStr,
         'Content-Type': 'application/x-www-form-urlencoded',
       },
->>>>>>> Develop
     },
   );
   // .then(res => res.json())
