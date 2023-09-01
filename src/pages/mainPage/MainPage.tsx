ce4f00b9140a9c21bc5b5a9bf1fe567cbab3c75a
Valeriya2511 <finsternis.ru@mail.ru>
2023-09-01 22:55:01 +0600

    feat: add main page

--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -8,11 +8,25 @@ export function MainPage() {
         Just a store
       </h1>
       <p className = {styles.greetings}>
-        Welcome to our simple online store! Our company welcomes everyone who visits the site!
+        ★ Welcome to our simple online store! Our company welcomes everyone who visits the site!
       </p>
       <p className = {styles.greetings}>
-        Our simple online store offers the lowest prices, don't even try to find cheaper!
+        ★ Our simple online store offers the lowest prices, don't even try to find cheaper!
       </p>
+      <p className = {styles.itemlist}>Our range</p>
+      <ul className={styles.list}>
+        <li className={`${styles.item} ${styles.jacket}`}></li>
+        <li className={`${styles.item} ${styles.bag}`}></li>
+        <li className={`${styles.item} ${styles.shirt}`}></li>
+        <li className={`${styles.item} ${styles.shoes}`}></li>
+        <li className={`${styles.item} ${styles.ring}`}></li>
+        <li className={`${styles.item} ${styles.scarf}`}></li>
+        <li className={`${styles.item} ${styles.wallet}`}></li>
+        <li className={`${styles.item} ${styles.belt}`}></li>
+        <li className={`${styles.item} ${styles.jeans}`}></li>
+        <li className={`${styles.item} ${styles.dress}`}></li>
+        <li className={`${styles.item} ${styles.glasses}`}></li>
+    </ul>
     </div>
   </div>
   )
