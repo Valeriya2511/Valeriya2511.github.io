e4a136570e843deaedfa2ec6460a04658d64a445
AlexDrags <ksander-88@yandex.ru>
2023-09-10 19:28:46 +0300

    fix:validate function

--- src/components/SignUpForm/SignUpForm.tsx
+++ src/components/SignUpForm/SignUpForm.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import InputField from '../UI/InputField/InputField';
 import ButtonAutorization from '../UI/ButtonAutorization/ButtonAutorization';
 import { useAutorization } from '../../hooks/useAutorization/useAutorization';
@@ -9,6 +9,7 @@ import { AuthContext } from '../../context/authContext/AuthContext';
 import { Navigate } from 'react-router-dom';
 
 export default function SignUpForm() {
+  const [isDisabled, setIsDisabled] = useState(true);
   const { submitHandler } = useAutorization();
   const { isAuth, setIsAuth } = useContext(AuthContext);
   if (isAuth) {
@@ -19,22 +20,23 @@ export default function SignUpForm() {
       onSubmit={async event => {
         const userData = await submitHandler(event);
         const tokenData = await getToken();
-        const { access_token, token_type } = await tokenData.json();
+        const { access_token } = await tokenData.json();
         await postClient(userData, access_token);
         setIsAuth(true);
       }}
+      autoComplete="off"
     >
       <p>
-        <InputField type="text" name="name" placeholder="You name" />
+        <InputField type="text" name="name" placeholder="You name" isDisabled={setIsDisabled} />
       </p>
       <p>
-        <InputField type="email" name="email" placeholder="Email" />
+        <InputField type="email" name="email" placeholder="Email" isDisabled={setIsDisabled} />
       </p>
       <p>
-        <InputField type="password" name="password" placeholder="Password" />
+        <InputField type="password" name="password" placeholder="Password" isDisabled={setIsDisabled} />
       </p>
       <p>
-        <ButtonAutorization type="submit" children="Registration" />
+        <ButtonAutorization type="submit" children="Registration" disabled={isDisabled} />
       </p>
     </form>
   );
