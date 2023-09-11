d99e9fa05a4c74441cb0d45ab704f8dbcab0a6a1
error404-2 <kapwas@mail.ru>
2023-09-11 22:03:37 +0300

    feat: add Loading... to ProductPage

--- src/pages/productsPage/ProductsPage.tsx
+++ src/pages/productsPage/ProductsPage.tsx
@@ -2,12 +2,15 @@ import { useContext, useEffect, useState } from 'react';
 import { ProductCard } from '../../components/productCard/ProductCard';
 import styles from './ProductsPage.module.css';
 import { ProductsContext } from '../../context/productsContext/productsContext';
+import { CategoriesContext } from '../../context/categoriesContext/CategoriesContext';
 import { Sidebar } from '../../components/sidebar/Sidebar';
 import Pagination from '../../components/pagination/Pagination';
+import { Loading } from '../../components/loading/Loading';
 
 export function ProductsPage() {
   const [count, setCount] = useState(3);
   const { products } = useContext(ProductsContext);
+  const { categories } = useContext(CategoriesContext);
   useEffect(() => {
     //console.log(products);
   }, [products]);
@@ -24,8 +27,10 @@ export function ProductsPage() {
               return <ProductCard product={element} key={id} />;
             }
           })
-        ) : (
+        ) : categories.length ? (
           <img src="./SoldOut.jpg" alt="Sold Out"></img>
+        ) : (
+          <Loading />
         )}
       </div>
       <Pagination countItem={count} onClick={setCount} />
