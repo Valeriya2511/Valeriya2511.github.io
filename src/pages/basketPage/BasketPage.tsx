cfafad7b4b132b66d88469e0f720fab757aca6e5
Valeriya2511 <finsternis.ru@mail.ru>
2023-09-12 16:43:11 +0600

    feat: add basket-empty pages

--- src/pages/basketPage/BasketPage.tsx
+++ src/pages/basketPage/BasketPage.tsx
@@ -1,3 +1,15 @@
+import styles from './BasketPage.module.css'
+import { Link } from 'react-router-dom';
+
 export function BasketPage() {
-  return <div>BasketPage</div>;
+  return (
+  <div className={styles.basket}>
+    <div className={ styles.empty}>
+      <h2 className={ styles.title__empty}>Your basket is empty</h2>
+      <p className={styles.p__empty}>Please add an item to checkout</p>
+      <Link className={styles.link} to="/products">
+        <button className={styles.button__empty}>Go to catalog</button>
+      </Link>
+    </div>
+  </div>)
 }
