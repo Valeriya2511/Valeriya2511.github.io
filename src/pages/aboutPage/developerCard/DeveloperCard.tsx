c77cce82b5f4d1ff315beb2b75e25d1ebe81f9a5
error404-2 <kapwas@mail.ru>
2023-09-18 19:58:59 +0300

    feat: add Developers Data

--- src/pages/aboutPage/developerCard/DeveloperCard.tsx
+++ src/pages/aboutPage/developerCard/DeveloperCard.tsx
@@ -11,18 +11,22 @@ export function DeveloperCard(props: {
 }) {
   return (
     <div className={styles.mainCardContainer}>
-      <div className={styles.titleContainer}>{props.name}</div>
-      <div className={styles.roleContainer}>{props.role}</div>
-      <div className={styles.imageContainer}>
-        <img className={styles.imageDev} src={props.image} alt="DeveloperPhoto" />
+      <div className={styles.topWrapper}>
+        <div className={styles.titleContainer}>{props.name}</div>
+        <div className={styles.roleContainer}>{props.role}</div>
       </div>
-      <div className={styles.contributionContainer}>Contribution: {props.contribution}</div>
-      <div className={styles.colaborationMethodContainer}>{props.colaborationMethod}</div>
-      <div className={styles.shortBioContainer}>{props.shortBio}</div>
-      <div className={styles.linkContainer}>
-        <a className={styles.link} href={props.gitHub}>
-          Welcome to Github of {props.name}
-        </a>
+      <div className={styles.descriptionWrapper}>
+        <div className={styles.imageContainer}>
+          <img className={styles.imageDev} src={props.image} alt="DeveloperPhoto" />
+        </div>
+        <div className={styles.contributionContainer}>Contribution: {props.contribution}</div>
+        <div className={styles.colaborationMethodContainer}>{props.colaborationMethod}</div>
+        <div className={styles.shortBioContainer}>{props.shortBio}</div>
+        <div className={styles.linkContainer}>
+          <a className={styles.link} href={props.gitHub}>
+            Welcome to Github of {props.name}
+          </a>
+        </div>
       </div>
     </div>
   );
--- src/pages/aboutPage/developerCard/DevelopersData.ts
+++ src/pages/aboutPage/developerCard/DevelopersData.ts
@@ -9,25 +9,31 @@ export interface iDeveloper {
 }
 export const DevelopersData: iDeveloper[] = [
   {
-    name: 'FIO (Valeriya2511)',
-    role: 'string',
-    image: './Developer',
-    contribution: 'string',
-    colaborationMethod: 'string',
-    shortBio: 'string',
+    name: 'Valeriya Tikhonova (Valeriya2511)',
+    role: 'Team lead',
+    image: './Valery.jpg',
+    contribution: 'MainPage, Footer, Cart, API commercetools',
+    colaborationMethod: 'Scrum 2 weeks, 4sprints. Github, Discord, Trello, Figma, Postman',
+    shortBio: `I am 29 years old. I started studying at RS School! I worked as an accountant. 
+    I don't have any programming experience yet, but I want to become a front-end developer. 
+    I like to learn new things and strive for the goal. I also love coffee, cats and playing ps4.`,
     gitHub: 'https://github.com/Valeriya2511',
   },
   {
-    name: 'FIO (AlexDrags)',
-    role: 'string',
-    image: 'string',
-    contribution: 'string',
-    colaborationMethod: 'string',
-    shortBio: 'string',
+    name: 'Alexandr Kuralenko (AlexDrags)',
+    role: 'Developer',
+    image: './Alexandr.jpg',
+    contribution: `Components : Login / SugnUp form, ProductsPage
+    Custom hooks: useProducts, useAuthorization;
+    Application architecture + EcommerceApi;`,
+    colaborationMethod: 'Scrum 2 weeks, 4sprints. Github, Discord, Trello, Figma, Postman',
+    shortBio: `Graduated from Ivanovo State University of Trade and Economics - tourism manager;
+    Interactive online courses: Profession Front-end Developer;
+    I lead a healthy lifestyle, I like to visit new countries and learn something new.`,
     gitHub: 'https://github.com/AlexDrags',
   },
   {
-    name: 'Kapustin Vasiliy (Kap66/Error404-2)',
+    name: 'Vasiliy Kapustin (Kap66/Error404-2)',
     role: 'Developer',
     image: './Vasiliy.jpg',
     contribution: 'Eslint, Navigation, Catalog, ProductCard, PasswordFlow, Basket, About',
